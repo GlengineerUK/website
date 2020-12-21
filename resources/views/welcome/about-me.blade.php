1f8affc3e7de18f4266f406dfedb212299f3c161
GlengineerUK <glenuk@gmail.com>
2020-12-21 17:56:24 +0000

    Updated info

---
+++ resources/views/welcome/about-me.blade.php
@@ -0,0 +1,17 @@
+<p>Hello,</p>
+<p class="mt-3">I'm Glen, a software developer based in England with a background in training.</p>
+<p class="mt-3">I have two years’ professional experience, during which, I have confidently operated at all stages of the software development life cycle:</p>
+<ul class="list-disc ml-8 mt-3">
+    <li>Assisting with project and feature feasibility studies</li>
+    <li>Engaging with stakeholders to elicit requirements</li>
+    <li>Designing models using UML</li>
+    <li>Writing clean, maintainable code using paradigms such as MVC</li>
+    <li>Creating comprehensive testing suites that include unit, integration and feature tests</li>
+    <li>Configuring development resources, including setting up Linux web servers</li>
+    <li>Supporting users via incident management tickets, change requests and staff training & documentation</li>
+</ul>
+<p class="mt-3">Recently, I achieved a <span class="italic font-semibold">pass with distinction</span> on my Software Development Level 4 apprenticeship. I completed two significant full-stack projects, from planning through to maintenance. The first, a purchase order system to manage the companies purchase orders and improve visibility of spending; the second, a configuration management tool to catalogue and improve the visibility of our retail stores' information.</p>
+<p class="mt-3">I really enjoy learning new technologies and better ways to do things. Earlier this year, I containerised our Laravel development environment using docker and wrote the documentation required for it become the standard within our department.</p>
+<p class="mt-3">A day rarely goes by where I don’t spend at least an hour working on my professional development. Lately, I am progressing through the Web Development Screencasts at Laracasts as well as an online data science course from MITx at edx.com. I also enjoy watching talks from conferences such as GOTO, Laracon and many more.</p>
+<p class="mt-3">When I am not writing code, you will typically find me working on my computer, messing around with the latest development tools to improve my skillset or playing strategy and simulation games. Unless it’s game night, in which case I’ll be watching my favourite esports team, Fnatic.</p>
+<p class="mt-3">Thanks for taking the time to look around my website. Feel free to look at my qualifications and projects using the tabs above.</p>
---
+++ resources/views/welcome/qualifications.blade.php
@@ -0,0 +1,10 @@
+<h2 class="font-bold">Qualifications</h2>
+<a href="https://www.instituteforapprenticeships.org/apprenticeship-standards/software-developer">Software Developer Apprenticeship Level 4 with distinction</a>
+<h2 class="font-bold">Certifications</h2>
+<a href="{{asset('/images/certs/bcs_dip_sdmeth_l4.pdf')}}">BCS: Level 4 Diploma in Software Development Methodologies (603/0543/5)</a>
+<br>
+<a href="https://www.youracclaim.com/badges/79d1f84a-4a88-4537-92ac-576bdc22b125/public_url">MTA: HTML5 Application Development Fundamentals</a>
+<br>
+<a href="https://courses.edx.org/certificates/b918c3f6710b4c78a320989377ff668b">MITx: Introduction to Computer Science and Programming Using Python (6.00.1x)</a>
+<br>
+<a href="{{asset('/images/certs/acelos_itil_foundation.pdf')}}">Axelos: ITIL® 4 Foundation</a>
