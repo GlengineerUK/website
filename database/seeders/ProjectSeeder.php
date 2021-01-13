2112840024795565ec5b086677be47d7f136db82
Glen Clark <glenuk@gmail.com>
2021-01-13 22:09:13 +0000

    Removed Canvas

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -15,14 +15,25 @@ class ProjectSeeder extends Seeder
     public function run()
     {
         Project::create([
-        'title' => ,
-        'slug' => ,
-        'summary' => ,
-        'content' => ,
-        'tags' => ,
-        'link_url' => ,
-        'image_url' => ,
-        'published' => ,
-        ])
+            'title' => 'Portfolio Website',
+            'slug' => 'portfolio-website',
+            'summary' => '<p>The application poweri<span class="ql-font-monospace">ng this website. P</span>roviders a complete CRUD application to manage project and blog posts.</p><p><strong><u>More content</u></strong></p><pre class="ql-syntax" spellcheck="false">$v = $a + 1</pre><h1><strong><u>More content</u></strong></h1><p><br></p>',
+            'content' => '<p>Some content</p><p><br></p><p><span class="ql-size-large">More content</span></p><p><br></p><p><strong><u>More content</u></strong></p><pre class="ql-syntax" spellcheck="false">$v = $a + 1</pre><h1><strong><u>More content</u></strong></h1>',
+            'tags' => 'Laravel, Tailwindcss, Canvas, Apline.js, HTML5, CSS3, Javascript',
+            'link_url' => '',
+            'image_url' => '',
+            'published' => 1,
+        ]);
+
+        Project::create([
+            'title' => 'Staff Planner',
+            'slug' => 'staff-planner',
+            'summary' => '<p>A web application used to plan shifts and staffing levels.</p>',
+            'content' => '<p>Some content</p>',
+            'tags' => 'Laravel, Tailwindcss, HTML5, CSS3, Javascript, Vue.js',
+            'link_url' => '',
+            'image_url' => '',
+            'published' => 1,
+        ]);
     }
 }
