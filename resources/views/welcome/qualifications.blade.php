46a626b892b6652755b1589d31c6326991c6e6c6
gclark18 <glenuk@gmail.com>
2021-01-26 11:01:48 +0000

    Removed Canvas

--- resources/views/welcome/qualifications.blade.php
+++ resources/views/welcome/qualifications.blade.php
@@ -1,10 +1,17 @@
 <h2 class="font-bold">Qualifications</h2>
-<a href="{{asset('/images/certs/soft_dev_l4.pdf')}}">Software Developer Apprenticeship Level 4 with distinction</a>
+<ul>
+    <li><a href="{{asset('/images/certs/soft_dev_l4.pdf')}}">Software Developer Apprenticeship Level 4 with distinction</a></li>
+</ul>
 <h2 class="font-bold">Certifications</h2>
-<a href="{{asset('/images/certs/bcs_dip_sdmeth_l4.pdf')}}">BCS: Level 4 Diploma in Software Development Methodologies (603/0543/5)</a>
-<br>
-<a href="https://www.youracclaim.com/badges/79d1f84a-4a88-4537-92ac-576bdc22b125/public_url">MTA: HTML5 Application Development Fundamentals</a>
-<br>
-<a href="https://courses.edx.org/certificates/b918c3f6710b4c78a320989377ff668b">MITx: Introduction to Computer Science and Programming Using Python (6.00.1x)</a>
-<br>
-<a href="{{asset('/images/certs/acelos_itil_foundation.pdf')}}">Axelos: ITIL® 4 Foundation</a>
+<ul>
+    <li><a href="{{asset('/images/certs/bcs_dip_sdmeth_l4.pdf')}}">BCS: Level 4 Diploma in Software Development Methodologies (603/0543/5)</a></li>
+    <li><a href="https://www.youracclaim.com/badges/79d1f84a-4a88-4537-92ac-576bdc22b125/public_url">MTA: HTML5 Application Development Fundamentals</a></li>
+    <li><a href="https://courses.edx.org/certificates/b918c3f6710b4c78a320989377ff668b">MITx: Introduction to Computer Science and Programming Using Python (6.00.1x)</a></li>
+    <li><a href="{{asset('/images/certs/acelos_itil_foundation.pdf')}}">Axelos: ITIL® 4 Foundation</a></li>
+</ul>
+<h2 class="font-bold">Professional Memberships</h2>
+<ul>
+    <li><a href="https://www.bcs.org/membership/get-registered/professional-registration-for-it-technicians-rittech/register-of-it-technicians/">RITTech: Register of professional IT technicians</a></li>
+    <li><a href="https://www.bcs.org/membership/become-a-member/associate-member-criteria/">British Computer Society (BCS): Associate member</a></li>
+</ul>
+
